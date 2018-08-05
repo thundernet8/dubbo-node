9f6e28aa0fe0aa29efb94d0459bffaffde657f66
wuxueqian <wuxueqian@meituan.com>
2018-08-05 16:37:43 +0800

    v0.2.6

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,36 +1,33 @@
-import { resolve, dirname, extname } from "path";
-import { readFileSync, writeFileSync, readdirSync } from "fs";
-import genServices from "./genServices";
-import * as mkdirp from "mkdirp";
-import * as yargs from "yargs";
+import { resolve, dirname, extname } from 'path';
+import { readFileSync, writeFileSync, readdirSync } from 'fs';
+import gen from './genServices';
+import * as mkdirp from 'mkdirp';
+import * as yargs from 'yargs';
 
-const version = require("../../package.json").version;
+const version = require('../../package.json').version;
 
 const argv = yargs
     .version(version)
-    .alias("v", "version")
-    .alias("i", "input")
-    .alias("o", "output")
-    .alias("g", "serviceGroup")
-    .alias("r", "serviceVerison")
-    .alias("t", "serviceTimeout")
-    .alias("h", "help")
-    .usage("Usage: $0 <command> [options]")
-    .example(
-        "$0 -i ./DemoService.java -o ./services -g dubbo -r LATEST -t 6000",
-        ""
-    )
-    .demandOption(["i"])
-    .default("o", "./services")
-    .default("g", "dubbo")
-    .default("r", "LATEST")
-    .default("t", 6000)
-    .describe("i", "Input Java file(s) path(directory)")
-    .describe("o", "Ouput typescript file folder")
-    .describe("g", "Service group in zookeeper/dubbo")
-    .describe("r", "Service version/revision in zookeeper/dubbo")
-    .describe("t", "Service timeout in zookeeper/dubbo")
-    .epilog("Copyright " + new Date().getFullYear())
+    .alias('v', 'version')
+    .alias('i', 'input')
+    .alias('o', 'output')
+    .alias('g', 'serviceGroup')
+    .alias('r', 'serviceVerison')
+    .alias('t', 'serviceTimeout')
+    .alias('h', 'help')
+    .usage('Usage: $0 <command> [options]')
+    .example('$0 -i ./DemoService.java -o ./services -g dubbo -r LATEST -t 6000', '')
+    .demandOption(['i'])
+    .default('o', './services')
+    .default('g', 'dubbo')
+    .default('r', 'LATEST')
+    .default('t', 6000)
+    .describe('i', 'Input Java file(s) path(directory)')
+    .describe('o', 'Ouput typescript file folder')
+    .describe('g', 'Service group in zookeeper/dubbo')
+    .describe('r', 'Service version/revision in zookeeper/dubbo')
+    .describe('t', 'Service timeout in zookeeper/dubbo')
+    .epilog('Copyright ' + new Date().getFullYear())
     .help().argv;
 
 try {
@@ -38,12 +35,12 @@ try {
     mkdirp(outputFoloder);
 
     const files: string[] = [];
-    if (extname(argv.i).toLowerCase() === ".java") {
+    if (extname(argv.i).toLowerCase() === '.java') {
         files.push(resolve(argv.i));
     } else {
-        const inputFolder = dirname(resolve(argv.i + "/x.x"));
+        const inputFolder = dirname(resolve(argv.i + '/x.x'));
         readdirSync(inputFolder).forEach(f => {
-            if (f.toLowerCase().endsWith(".java")) {
+            if (f.toLowerCase().endsWith('.java')) {
                 files.push(resolve(inputFolder, f));
             }
         });
@@ -51,14 +48,18 @@ try {
 
     files.forEach(f => {
         const code = readFileSync(f).toString();
-        console.log("\r\n");
-        genServices(code, argv.g, argv.r, argv.t).forEach(s => {
+        console.log('\r\n');
+        gen.genServices(code, argv.g, argv.r, argv.t).forEach(s => {
             const outputFile = resolve(outputFoloder, `${s.name}.ts`);
             console.log(outputFile);
             writeFileSync(outputFile, s.code);
         });
     });
-    console.log("\r\n");
+    console.log('\r\n');
+
+    gen.genIndexFile();
+
+    console.log('\r\n');
 } catch (err) {
     throw err;
 }
